d95d2d43b9fc11aa68105afe4992fe1ccf80b19a
EvanGertis <physevangertis@gmail.com>
2020-03-01 11:06:23 -0500

    fixed up problem 4

--- homework/assignment_05/A5p4.sh
+++ homework/assignment_05/A5p4.sh
@@ -4,7 +4,6 @@ i=$1
 while [ $i -lt 4 ] ;
 do
         j=$2
-        echo "$i*$j=$((i*j))"
         while [ $j -lt 6 ] ;
         do
                 echo "$i*$j=$((i*j))"
@@ -14,4 +13,4 @@ do
 done
 }
 
-printProduct 1 2
+printProduct 1 1
