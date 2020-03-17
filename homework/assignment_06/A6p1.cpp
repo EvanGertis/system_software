1e5a92cce96e01fabf710ec788c02f4535f38526
EvanGertis <physevangertis@gmail.com>
2020-03-17 08:49:36 -0400

    added substring partitioning

--- homework/assignment_06/A6p1.cpp
+++ homework/assignment_06/A6p1.cpp
@@ -2,10 +2,11 @@
 #include <iostream>
 #include <cstdlib>
 #include <exception>
+#include <string>
 
 void *proc(void *arg){
-	int *integer = (int *)arg;
-	std::cout << "now printing " << *integer << std::endl;
+	std::string substring = *(std::string*)arg;
+	std::cout << substring << std::endl;
 }
 
 void printUsgae(char * programName){
@@ -38,12 +39,25 @@ int main(int argc, char * argv[]){
 	
 	pthread_t id[n];
 	int r[n];
-
-	for(int i=0; i < n; i++){
-		int * value = (int *)malloc(sizeof(int));
-		*value = i;
-		r[i] = pthread_create( &id[i], NULL, proc, (void*) value);
-	}
+	std::string key = "ABCDEFGHIJKLMNOPQRS";
+	std::string::iterator it;	
+	int i = 0;
+	for(it=key.begin(); it != key.end(); it++){
+		if(i  % n == 0){
+			if( (i+n) < (int)key.capacity()){
+				std::string substring;
+				substring = key.substr(i,n);
+				//std::cout << substring << std::endl;
+				r[i] = pthread_create( &id[i], NULL, proc, (void *)&substring);
+			}else{
+				std::string substring;
+				substring = key.substr(i,(key.capacity() - i));
+				//std::cout << substring << std::endl;
+				r[i] = pthread_create( &id[i], NULL, proc, (void *)&substring);
+			}
+		}
+		i++;
+	}		
 	
 	for(int i=0; i < n; i++){
 		pthread_join(id[i], NULL);	
